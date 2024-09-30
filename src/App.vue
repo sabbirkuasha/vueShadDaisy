dd1cda6a1540aad38d5f4bfb8147b6e577cf512b
sabbirz <6649698+sabbirkuasha@users.noreply.github.com>
2024-10-01 00:13:41 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Button } from '@/components/ui/button'
+
+
+import { Calendar } from '@/components/ui/calendar'
+
+// const value = ref(today(getLocalTimeZone())) 
+</script>
+
+<template>
+  <div class="p-4 flex flex-col gap-5">
+
+    <h1 class="text-3xl border p-2">Hello World</h1>
+    <h2 class="ring p-4">Hey</h2>
+    <div>
+      <Button>Click me</Button>
+    </div>
+  </div>
+
+  <div class="flex p-4">
+    <Calendar v-model="value" :weekday-format="'short'" class="rounded-md border" />
+  </div>
+
+  <div class="p-4">
+    <button class="btn btn-primary">Hello</button>
+  </div>
+
+</template>
---
+++ vite.config.js
@@ -0,0 +1,41 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+// import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
